f697cd7671adb97db5dab42592368ae6c26a76d6
bazai hassan <bazaee.hassan@gmail.com>
2025-01-16 11:36:18 +0330

    tween

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="w-full flex flex-col mx-auto container gap-3 mt-2">
+        <p class="text-4xl text-center text-black font-extrabold">Nuxt GSAP Examples</p>
+        <NuxtLink to="/tween">
+            <p class="text-2xl border font-bold text-blue-600 hover:shadow-lg p-2" >1 - Tween</p>
+        </NuxtLink>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/tween.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex items-center justify-center h-screen w-full">
+    <img id="img" class="w-64 h-64" src="~/assets/images/img.webp" alt="" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import gsap from "gsap";
+
+function tween(
+  duration: number,
+  delay: number,
+  rotate: number,
+  skew: number,
+  ease: string
+) {
+  gsap.to("#img", {
+    duration: duration,
+    delay: delay,
+    rotate: rotate,
+    skew: skew,
+    ease: ease,
+  });
+}
+
+onMounted(async () => {
+    tween(3,1,2880,45,"power1.in")
+});
+</script>
